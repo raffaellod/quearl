1287e95117ba7b848b046153437e3ce8df537bd2
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2013-08-21 13:51:27 -0400

    Simplify and speed up QlApplication
    Reduce the number of methods to decrease complexity; improve readability
    and speed of read/write from/to disk code.
    
    Also fix a potential undesirable side effect of QlApplication::reload()
    failures.

--- quearl/module/core/main-application.inc.php
+++ quearl/module/core/main-application.inc.php
@@ -66,7 +66,14 @@ class QlApplication {
 		$this->m_sFileName = $_APP['core']['rwdata_lpath'] . 'core/app.dat';
 		$this->m_sLockFileName = $_APP['core']['lock_lpath'] . 'app.dat.lock';
 		$this->m_cLocks = 0;
-		$this->read();
+		# Set size and CRC to provide a never-matching reference for QlApplicaion::unlock().
+		$this->m_cb = 0;
+		$this->m_iCRC = 0;
+
+		# Try and load from persistent storage.
+		$this->reload();
+
+		$GLOBALS['ql_app'] = $this;
 	}
 
 
@@ -78,7 +85,7 @@ class QlApplication {
 			$this->m_cLocks = 1;
 			$this->unlock();
 		}
-		unset($GLOBALS['_APP']);
+		unset($GLOBALS['ql_app'], $GLOBALS['_APP']);
 	}
 
 
@@ -173,28 +180,29 @@ class QlApplication {
 	}
 
 
-	## Reads $_APP from persistent storage.
+	## Reads $_APP from persistent storage. Other than being invoked when $ql_app is constructed, it
+	# can be called at any time to reload $_APP, which can be useful for scripts that need to make
+	# sure they’re using the latest $_APP after running for an extended amount of time.
 	#
 	# bool return
-	#    true if $_APP was loaded from persistent storage, or false if the latter was unavailable,
-	#    which means that $_APP has the default contents loaded from the configuration files.
+	#    true if $_APP was loaded from persistent storage, or false if the latter was unavailable, in
+	#    which case $_APP is unaffected.
 	#
-	private function read() {
-		global $_APP;
-		# Keep a backup of the default core section, so we can restore it in case something goes
-		# wrong.
-		$arrDefaultCore =& $_APP['core'];
+	private function reload() {
+		# Try and read from persistent storage.
 		$s = @file_get_contents($this->m_sFileName);
 		if ($s) {
-			$_APP = @unserialize($s);
-			if ($_APP !== false) {
+			# Keep a backup of the current $_APP, so we can restore it in case something goes wrong.
+			$arrApp = @unserialize($s);
+			if ($arrApp !== false) {
+				# A valid array was loaded; overwrite $_APP with that.
+				$GLOBALS['_APP'] =& $arrApp;
+				# Recalculate size and CRC for later.
 				$this->m_cb = strlen($s);
-				$this->m_iCRC = $_APP['__ql_CRC'];
-				unset($s, $_APP['__ql_CRC']);
+				$this->m_iCRC = crc32($s);
 				return true;
 			}
 		}
-
 		# Something went wrong.
 		# TODO: also e-mail an administrator?
 		ql_log(
@@ -202,56 +210,29 @@ class QlApplication {
 			'Application data ' . ($s === false ? 'missing' : 'corrupt') . ', defaults loaded!',
 			'<pre>' . ql_lenc($s) . '</pre>'
 		);
-
-		# Restore the default $_APP created in the constructor.
-		$_APP = array(
-			'core' => &$arrDefaultCore,
-		);
-		$this->m_cb = 0;
-		$this->m_iCRC = 0;
 		return false;
-	}
-
-
-	## Reloads $_APP. Useful for scripts that wish to reload $_APP after running for an extended
-	# amount of time.
-	#
-	public function reload() {
-		$this->read();
 	}
 
 
 	## Leaves the serialized context started with QlApplication::lock().
 	#
 	public function unlock() {
+		global $_APP;
 		if ($this->m_cLocks == 1) {
-			$this->write();
+			# Recalculate size and CRC of the serialized $_APP; if different, we’ll need to update the
+			# persistent storage.
+			$s = serialize($_APP);
+			$cb = strlen($s);
+			$iCRC = crc32($s);
+			if ($this->m_cb != $cb || $this->m_iCRC != $iCRC) {
+				$this->m_cb = $cb;
+				$this->m_iCRC = $iCRC;
+				file_put_contents($this->m_sFileName, $s);
+			}
 			fclose($this->m_fileLock);
-			@unlink($this->m_sLockFileName);
 			$this->m_fileLock = null;
 		}
 		--$this->m_cLocks;
-	}
-
-
-	## Writes $_APP to persistent storage.
-	#
-	# bool return
-	#    true if writing was successful or not necessary, false if it was necessary but failed.
-	#
-	private function write() {
-		global $_APP;
-		$s = serialize($_APP);
-		# Recalculate size and CRC of the serialized $_APP; if different, we’ll need to update the
-		# persistent storage.
-		if ($this->m_cb != ($cb = strlen($s)) || $this->m_iCRC != ($iCRC = crc32($s))) {
-			$this->m_cb = $cb;
-			$this->m_iCRC = (isset($iCRC) ? $iCRC : crc32($s));
-			$_APP['__ql_CRC'] = $this->m_iCRC;
-			file_put_contents($this->m_sFileName, serialize($_APP));
-			unset($_APP['__ql_CRC']);
-		}
-		return true;
 	}
 }
 
