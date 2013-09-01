2973da712d40affebf220a42b550fceb11d2d10a
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2013-09-01 00:48:44 -0400

    Spin quearl_inst.py off localize.py
    Refactor the non-l10n-specific code to be reusable, and move it to a
    separate file so it can be pulled in by other scripts.

--- quearl/bin/localize.py
+++ quearl/bin/localize.py
@@ -30,75 +30,50 @@ import sys
 # l10n_generator
 
 class l10n_generator(object):
-	"""Generates localization files for all languages from the localized string files."""
+	"""Generates localization files for all programming languages from the localized string files."""
 
-	def __init__(self, sQuearlDir):
-		"""Constructor.
-
-		str sQuearlDir
-			Quearl installation subdirectory, e.g. the directory containing the “module” directory;
-			default: “Quearl”.
-		"""
-
-		self.m_sModulesDir = os.path.join(sQuearlDir, 'module')
-		# TODO: FIXME: temp hack!
-		self.m_sRODataDir = os.path.normpath(os.path.join(self.m_sModulesDir, '../data.ro'))
-
-
-	def update_all_modules(self):
-		"""Updates every localization file in every module."""
-
-		for sFileName in os.listdir(self.m_sModulesDir):
-			sFileName = os.path.join(self.m_sModulesDir, sFileName)
-			if os.path.isdir(sFileName):
-				self.update_module(sFileName)
-
-
-	def update_module(self, sModuleDir):
+	@classmethod
+	def update_module(cls, module):
 		"""Updates every localization file in a given module.
 
-		str sModuleDir
-			Full path to the directory containing the module. Note that its basename is not necessarily
-			the module’s name.
+		quearl_module module
+			Module for which to update the localization.
 		"""
 
-		sL10nDir = os.path.join(sModuleDir, 'l10n')
-		if not os.path.isdir(sL10nDir):
-			# Maybe this module has no localization.
+		sL10nDir = module.l10n_dir()
+		if sL10nDir == None:
+			# This module has no localization files.
 			return
 
-		# sModulePrefix like “CORE”, “ECOMM”, …
-		# TODO: FIXME: temp hack!
-		sModulePrefix = os.path.basename(sModuleDir).upper() + '_'
 		for sFileName in os.listdir(sL10nDir):
 			if sFileName.endswith('.l10n'):
 				sFileName = os.path.join(sL10nDir, sFileName)
 				if os.path.isfile(sFileName):
 					sys.stdout.write('Processing l10n file {}\n'.format(sFileName))
-					self.update_from_l10n_file(sFileName, sModulePrefix)
+					cls.update_from_l10n_file(module, sFileName)
 
 
-	def update_from_l10n_file(self, sL10nFileName, sModulePrefix):
+	@classmethod
+	def update_from_l10n_file(cls, module, sL10nFileName):
 		"""Updates the localization files originating from the specified l10n file.
 
+		quearl_module module
+			Module to which the file belongs.
 		str sL10nFileName
 			Full path to the .l10n file.
-		str sModulePrefix
-			Computed module prefix, ready to be used to generate constant named; must include a
-			trailing “_”.
 		"""
 
-		# Find out directory and locale from the l10n file name.
+		sModulePrefix = module.abbr().upper() + '_'
+
+		# Find out the locale from the l10n file name.
 		match = re.search(
-			'[\\/](?P<moduledir>[^\\/]+)[\\/]l10n[\\/](?P<locale>[^\\/]+)\.l10n$', sL10nFileName
+			'[\\/]l10n[\\/](?P<locale>[^\\/]+)\.l10n$', sL10nFileName
 		)
-		# We could have the caller provide this, but it’s not necessary.
-		sModuleDirName = match.group('moduledir')
 		sLocale = match.group('locale')
 
 		dictL10nEntries = {}
 		dictL10nEntries[sModulePrefix + 'L10N_INCLUDED'] = True
-		with open(sL10nFileName) as fileL10n:
+		with open(sL10nFileName, 'r') as fileL10n:
 			iLine = 0
 			# Parse through the whole file.
 			for sLine in fileL10n:
@@ -127,7 +102,7 @@ class l10n_generator(object):
 		dtL10nFile = os.path.getmtime(sL10nFileName)
 
 		for sType in 'php', 'js':
-			sOutputDir = os.path.join(self.m_sRODataDir, sModuleDirName, 'l10n', sType)
+			sOutputDir = os.path.join(module.rodata_dir(), 'l10n', sType)
 			# Make sure the destination directory exists.
 			try:
 				os.makedirs(sOutputDir, 0o755, True)
@@ -137,13 +112,13 @@ class l10n_generator(object):
 			# Try to get the last modification time of the output file.
 			try:
 				dtOutputFile = os.path.getmtime(sOutputFileName)
-			except:
+			except OSError:
 				# Probably the file doesn’t exist.
 				dtOutputFile = None
 			# If the file needs to be (re-)generated, go ahead.
 			if dtOutputFile == None or dtL10nFile > dtOutputFile:
 				sys.stdout.write('Updating {} file {}\n'.format(sType, sOutputFileName))
-				sFile = getattr(self, 'l10n_to_' + sType)(dictL10nEntries)
+				sFile = getattr(cls, 'l10n_to_' + sType)(dictL10nEntries)
 				with open(sOutputFileName, 'w') as f:
 					f.write(sFile)
 
@@ -164,7 +139,10 @@ class l10n_generator(object):
 			 '\n'
 		for sName, oValue in dictL10nEntries.items():
 			if isinstance(oValue, bool):
-				sValue = oValue and 'true' or 'false'
+				if oValue:
+					sValue = 'true'
+				else:
+					sValue = 'false'
 			elif isinstance(oValue, str):
 				# Escape escape sequences.
 				sValue = oValue.replace('\\', '\\\\').replace('\n', '\\n')
@@ -212,11 +190,15 @@ class l10n_generator(object):
 if __name__ == '__main__':
 	# Get the full path of this script.
 	sDir = os.path.dirname(os.path.abspath(sys.argv[0]))
-	# Obtain the Queal installation subdirectory.
-	sDir = os.path.normpath(os.path.join(sDir, '..'))
+	# Setup the PATH environment variable to load quearl_inst.
+	sys.path.append(sDir)
+	import quearl_inst
 
-	# Instantiate and launch the l10n_generator.
-	gen = l10n_generator(sDir)
-	gen.update_all_modules()
+	# Obtain the Quearl installation subdirectory and instantiate a quearl_inst for it.
+	qinst = quearl_inst.quearl_inst(os.path.normpath(os.path.join(sDir, '..')))
+	# Update all modules.
+	for module in qinst.modules():
+		l10n_generator.update_module(module)
+
 	sys.exit(0)
 
---
+++ quearl/bin/quearl_inst.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+# -*- coding: utf-8; mode: python; tab-width: 3 -*-
+#---------------------------------------------------------------------------------------------------
+# Quearl
+# Copyright 2013 Raffaello D. Di Napoli
+#---------------------------------------------------------------------------------------------------
+# This file is part of Quearl.
+#
+# Quearl is free software: you can redistribute it and/or modify it under the terms of the GNU
+# Affero General Public License as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Quearl is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Quearl. If
+# not, see <http://www.gnu.org/licenses/>.
+#---------------------------------------------------------------------------------------------------
+
+"""Classes to manage a Quearl installation."""
+
+import os
+
+
+
+####################################################################################################
+# quearl_inst
+
+class quearl_inst(object):
+	"""Provides access to a Quearl installation for maintenance purposes."""
+
+	def __init__(self, sQuearlDir):
+		"""Constructor.
+
+		str sQuearlDir
+			Quearl installation subdirectory, e.g. the directory containing the “module” directory;
+			default: “Quearl”.
+		"""
+
+		self._m_sModulesDir = os.path.join(sQuearlDir, 'module')
+		# TODO: FIXME: read this from module/core/module.conf file!
+		self._m_sRODataDir = os.path.normpath(os.path.join(self._m_sModulesDir, '../data.ro'))
+
+
+	def modules(self):
+		"""Iterates over every module."""
+
+		for sFileName in os.listdir(self._m_sModulesDir):
+			sFileName = os.path.join(self._m_sModulesDir, sFileName)
+			if os.path.isdir(sFileName):
+				yield quearl_module(self, sFileName)
+
+
+	def rodata_dir(self):
+		"""Returns the Quearl-wide read-only data directory.
+
+		str return
+			Directory.
+		"""
+
+		return self._m_sRODataDir
+
+
+
+####################################################################################################
+# quearl_module
+
+class quearl_module(object):
+	"""Quearl module installation directory."""
+
+	def __init__(self, qinst, sBaseDir):
+		"""Constructor.
+
+		quearl_inst qinst
+			Provides access to the Quearl installation.
+		str sBaseDir
+			Base directory of the module.
+		"""
+
+		# _m_sAbbr like “core”, “ecomm”, …
+		# TODO: FIXME: read this by parsing the module’s main.php!
+		self._m_sAbbr = os.path.basename(sBaseDir)
+		self._m_sBaseDir = sBaseDir
+		self._m_sL10nDir = os.path.join(sBaseDir, 'l10n')
+		if not os.path.isdir(self._m_sL10nDir):
+			# Maybe this module has no localization.
+			self._m_sL10nDir = None
+		self._m_sRODataDir = os.path.join(qinst.rodata_dir(), self._m_sAbbr)
+
+
+	def abbr(self):
+		"""Returns the module’s abbreviated name.
+
+		str return
+			Abbreviation.
+		"""
+
+		return self._m_sAbbr
+
+
+	def base_dir(self):
+		"""Returns the module’s base directory.
+
+		str return
+			Base directory.
+		"""
+
+		return self._m_sBaseDir
+
+
+	def l10n_dir(self):
+		"""Returns the module’s localization files directory.
+
+		str return
+			Localization files directory, or None if non-existent.
+		"""
+		return self._m_sL10nDir
+
+
+	def rodata_dir(self):
+		"""Returns the module’s read-only data directory.
+
+		str return
+			Directory.
+		"""
+
+		return self._m_sRODataDir
+
+
+
+####################################################################################################
+# __main__
+
+if __name__ == '__main__':
+	import sys
+
+	# Get the full path of this script.
+	sDir = os.path.dirname(os.path.abspath(sys.argv[0]))
+	# Obtain the Quearl installation subdirectory.
+	sDir = os.path.normpath(os.path.join(sDir, '..'))
+	# Instantiate a quearl_inst.
+	qinst = quearl_inst(sDir)
+
+	sys.exit(0)
+
